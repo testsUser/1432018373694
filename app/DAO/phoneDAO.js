8ab220a6d1c3da831de9673f458d4cc487ee94ae
Marcin Pyteraf <pyteraf@gmail.com>
2015-05-19 13:45:44 +0200

    task4 ready

--- app/DAO/phoneDAO.js
+++ app/DAO/phoneDAO.js
@@ -13,21 +13,38 @@
 
     function createNewOrUpdate(phone)
     {
-
+        var filter = {_id: phone._id || null};
+        return Model.findOneAndUpdateQ(filter, phone, {upsert: true}).then(function (result)
+        {
+            return {results: result};
+        });
     }
 
     function search(query)
     {
+        var pattern = query.query || '';
+        var filter = { $or: [
+            {model: {$regex: pattern, $options: 'i'}},
+            {brand: {$regex: pattern, $options: 'i'}},
+            {stan: {$regex: pattern, $options: 'i'}}
+        ]};
+        return Model.findQ(filter).then(function (result)
+        {
+            return {results: result};
+        });
     }
 
     function getDetails(phoneId)
     {
-
+        return Model.findOneQ({_id: phoneId}).then(function (result)
+        {
+            return {results: result};
+        });
     }
 
     function removePhone(phoneId)
     {
-
+        return Model.findOneAndRemoveQ({_id: phoneId});
     }
 
     module.exports = {
